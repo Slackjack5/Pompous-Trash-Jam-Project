3cabc3e44bfaec240121424a5411e6e2c2c83b30
Nycelly <dkr264@nyu.edu>
2021-07-10 19:11:37 -0400

    Created a Worm Hole Box and Explosive Box

--- Pompous Trash Jam Unity/Assets/Scripts/BoxDestruction.cs
+++ Pompous Trash Jam Unity/Assets/Scripts/BoxDestruction.cs
@@ -7,8 +7,15 @@ public class BoxDestruction : MonoBehaviour
   [SerializeField] private int maxHealth = 3;
   [SerializeField] private float hitForce = 10f;
 
-  public GameObject destructable;
+    public GameObject destructable;
+    public GameObject Wormhole;
+    public bool Explosive;
+    public bool gravityBox;
 
+    public float fieldofImpact;
+    public float force;
+    public float torque;
+    [SerializeField] Vector2 forceDirection;
   private Rigidbody2D rb;
 
   private int currentHealth;
@@ -32,14 +39,49 @@ public class BoxDestruction : MonoBehaviour
     }
   }
 
-  public void Destroy()
-  {
-    Instantiate(destructable, transform.position, Quaternion.identity);
-    DestroyGameObject();
-  }
+    public LayerMask LayerToHit;
 
   private void DestroyGameObject()
   {
     Destroy(gameObject);
   }
+
+    private void Explosion()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldofImpact, LayerToHit);
+        foreach (Collider2D obj in objects)
+        {
+            float randTorque = Random.Range(-25, 25);
+            Vector2 direction = obj.transform.position - transform.position;
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+            obj.GetComponent<Rigidbody2D>().AddForce(transform.up * force/2);
+            obj.GetComponent<Rigidbody2D>().AddTorque(randTorque);
+        }
+    }
+
+    public void Destroy()
+    {
+        Instantiate(destructable, transform.position, Quaternion.identity);
+        if (Wormhole)
+        {
+            Instantiate(Wormhole, new Vector2(transform.position.x,transform.position.y+2), Quaternion.identity);
+        }
+        DestroyGameObject();
+    }
+
+    //Debugging Code
+    private void OnMouseDown()
+    {
+        if (Explosive)
+        {
+            Explosion();
+        }
+        Destroy();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, fieldofImpact);
+    }
 }
