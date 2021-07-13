f3f9794bdd5cb7797696b234537dec4c81c42120
Aljon Pineda <aljonjbpineda@gmail.com>
2021-07-12 23:25:46 -0400

    Disable tube when its minigame is ready

--- Pompous Trash Jam Unity/Assets/Scripts/VacuumController.cs
+++ Pompous Trash Jam Unity/Assets/Scripts/VacuumController.cs
@@ -7,6 +7,7 @@ public class VacuumController : MonoBehaviour
 {
   [SerializeField] private TrashCounter trashCounter;
   [SerializeField] private string trash = "Trash";
+  [SerializeField] private TubeMinigame tubeMinigame;
 
   Rigidbody2D bone;
   VisualEffect vfx;
@@ -15,12 +16,13 @@ public class VacuumController : MonoBehaviour
   {
     bone = GetComponent<Rigidbody2D>();
     vfx = GameObject.Find("SuctionVFX").GetComponent<VisualEffect>();
+    vfx.SetInt("SpawnRate", 0);
     vfx.SetVector2("VFXCenter", bone.position); // set center initially and do not change it
   }
 
   void FixedUpdate()
   {
-    if (Input.GetMouseButton(0))
+    if (GameManager.IsGameActive && !tubeMinigame.IsReady && Input.GetMouseButton(0))
     {
       vfx.SetInt("SpawnRate", 64);
     }
@@ -28,10 +30,15 @@ public class VacuumController : MonoBehaviour
     {
       vfx.SetInt("SpawnRate", 0);
     }
+
     Vector2 mousePos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
-    vfx.SetFloat("VFXAngle", Mathf.Deg2Rad * (bone.rotation + 90));
-    Vector2 normal = (mousePos - bone.position).normalized;
-    bone.AddForce(normal * 500);
+
+    if (GameManager.IsGameActive && !tubeMinigame.IsReady)
+    {
+      vfx.SetFloat("VFXAngle", Mathf.Deg2Rad * (bone.rotation + 90));
+      Vector2 normal = (mousePos - bone.position).normalized;
+      bone.AddForce(normal * 500);
+    }
   }
 
   void OnCollisionEnter2D(Collision2D col)
