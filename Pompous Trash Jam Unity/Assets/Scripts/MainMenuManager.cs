62b85d0453f94d3843355ca09d4f082e4a610fb4
Aljon Pineda <aljonjbpineda@gmail.com>
2021-07-13 21:22:04 -0400

    Add listeners to Play and Quit buttons

--- Pompous Trash Jam Unity/Assets/Scripts/MainMenuManager.cs
+++ Pompous Trash Jam Unity/Assets/Scripts/MainMenuManager.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public void Play()
+  {
+    SceneManager.LoadScene(1);
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public void Quit()
+  {
+#if UNITY_EDITOR
+    UnityEditor.EditorApplication.isPlaying = false;
+#else
+    Application.Quit();
+#endif
+  }
 }
