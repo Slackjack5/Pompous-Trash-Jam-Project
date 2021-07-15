30d8276c75c76fd6e44ae1027327749e60edce3f
Zach <zlschect@gmail.com>
2021-07-15 10:22:11 -0400

    Implemented all visual effects for exploding box
    :)

--- Pompous Trash Jam Unity/Assets/Scripts/BoxDestruction.cs
+++ Pompous Trash Jam Unity/Assets/Scripts/BoxDestruction.cs
@@ -11,6 +11,7 @@ public class BoxDestruction : PhysicsObject
   public GameObject destructable;
   public GameObject Wormhole;
   public GameObject Box;
+  public GameObject explosionVFX;
   public bool Explosive;
   public bool gravityBox;
   public bool infinityBox;
@@ -24,8 +25,8 @@ public class BoxDestruction : PhysicsObject
 
   protected override void Start()
   {
+    Shader.SetGlobalFloat("_ShockTime", -99999);
     base.Start();
-
     currentHealth = maxHealth;
   }
 
@@ -45,50 +46,52 @@ public class BoxDestruction : PhysicsObject
   {
     Destroy(gameObject);
   }
-    private void Explosion()
+
+  private void Explosion()
+  {
+    Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldofImpact, LayerToHit);
+    foreach (Collider2D obj in objects)
     {
-        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldofImpact, LayerToHit);
-        foreach (Collider2D obj in objects)
-        {
-            float randTorque = Random.Range(-25, 25);
-            Vector2 direction = obj.transform.position - transform.position;
-            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
-            obj.GetComponent<Rigidbody2D>().AddForce(transform.up * force/2);
-            obj.GetComponent<Rigidbody2D>().AddTorque(randTorque);
-        }
+      float randTorque = Random.Range(-25, 25);
+      Vector2 direction = obj.transform.position - transform.position;
+      obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+      obj.GetComponent<Rigidbody2D>().AddForce(transform.up * force / 2);
+      obj.GetComponent<Rigidbody2D>().AddTorque(randTorque);
     }
+  }
+
   private void Destroy()
   {
     Instantiate(destructable, transform.position, Quaternion.identity);
     if (Explosive)
     {
-        Explosion();
+      Explosion();
     }
     if (gravityBox)
     {
-        Instantiate(Wormhole, new Vector2(transform.position.x,transform.position.y+2), Quaternion.identity);
+      Instantiate(Wormhole, new Vector2(transform.position.x, transform.position.y + 2), Quaternion.identity);
     }
     else if (infinityBox)
     {
-        Instantiate(Box, new Vector2(transform.position.x+.5f, transform.position.y + .5f), Quaternion.identity);
-        Instantiate(Box, new Vector2(transform.position.x - .5f, transform.position.y + .5f), Quaternion.identity);
-        Instantiate(Box, new Vector2(transform.position.x, transform.position.y + .5f), Quaternion.identity);
+      Instantiate(Box, new Vector2(transform.position.x + .5f, transform.position.y + .5f), Quaternion.identity);
+      Instantiate(Box, new Vector2(transform.position.x - .5f, transform.position.y + .5f), Quaternion.identity);
+      Instantiate(Box, new Vector2(transform.position.x, transform.position.y + .5f), Quaternion.identity);
     }
     else if (upgradeBox)
     {
-        int randNumber = Random.Range(0, 3);
-        if (randNumber==0)
-        {
-            Debug.Log("Spawned Power Up 1");
-        }
-        else if (randNumber == 1)
-        {
-            Debug.Log("Spawned Power Up 2");
-        }
-        else if (randNumber == 2)
-        {
-            Debug.Log("Spawned Power Up 3");
-        }
+      int randNumber = Random.Range(0, 3);
+      if (randNumber == 0)
+      {
+        Debug.Log("Spawned Power Up 1");
+      }
+      else if (randNumber == 1)
+      {
+        Debug.Log("Spawned Power Up 2");
+      }
+      else if (randNumber == 2)
+      {
+        Debug.Log("Spawned Power Up 3");
+      }
     }
 
     StartCoroutine(Freeze());
@@ -99,18 +102,24 @@ public class BoxDestruction : PhysicsObject
     GameManager.DeactivateGame();
     yield return new WaitForSeconds(freezeTime);
     GameManager.ActivateGame();
-
+    if (Explosive)
+    {
+      Shader.SetGlobalFloat("_ShockTime", Time.time);
+      Vector2 focalPoint = new Vector2(Camera.main.WorldToScreenPoint(transform.position, Camera.main.stereoActiveEye).x / Screen.width, Camera.main.WorldToScreenPoint(transform.position, Camera.main.stereoActiveEye).y / Screen.height);
+      Shader.SetGlobalVector("_FocalPoint", focalPoint);
+      Instantiate(explosionVFX, new Vector2(transform.position.x, transform.position.y), Quaternion.identity);
+    }
     DestroyGameObject();
   }
 
   //Debugging Code
   private void OnMouseDown()
-    {
-        Destroy();
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, fieldofImpact);
-    }
+  {
+    Destroy();
+  }
+  private void OnDrawGizmosSelected()
+  {
+    Gizmos.color = Color.green;
+    Gizmos.DrawWireSphere(transform.position, fieldofImpact);
+  }
 }
