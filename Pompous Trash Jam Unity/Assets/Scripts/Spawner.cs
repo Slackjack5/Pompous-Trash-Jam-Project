15dd6f5ce8cbb6f2a46ac02fcd642d7f4629b534
Aljon Pineda <aljonjbpineda@gmail.com>
2021-07-19 21:58:16 -0400

    Set spawn rates of each type of box

--- Pompous Trash Jam Unity/Assets/Scripts/Spawner.cs
+++ Pompous Trash Jam Unity/Assets/Scripts/Spawner.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-  [SerializeField] private GameObject[] boxes;
+  [SerializeField] private BoxSpawner[] boxes;
+  [SerializeField] private GameObject defaultBox;
   [SerializeField] private float spawnWidth = 12f;
   [SerializeField] private float spawnInterval = 1.5f;
 
+  [System.Serializable]
+  private struct BoxSpawner
+  {
+    [SerializeField] internal GameObject box;
+    [SerializeField, Range(0, 1f)] internal float spawnRate;
+  }
+
+  private struct CumulativeBoxSpawner
+  {
+    internal GameObject box;
+    internal float cumulativeSpawnRate;
+  }
+
   private bool isWaitingSpawn;
-  private List<GameObject> spawnOptions;
+  private List<CumulativeBoxSpawner> spawnOptions;
+  private float totalSpawnRate;
 
   private void Start()
   {
-    spawnOptions = new List<GameObject>();
+    spawnOptions = new List<CumulativeBoxSpawner>();
   }
 
   // Update is called once per frame
@@ -27,15 +42,26 @@ public class Spawner : MonoBehaviour
 
   private void EvaluateOptions()
   {
-    foreach (GameObject box in boxes)
+    // Rather than normalize all spawn rates to add up to 1, total the spawn rates
+    // and use that total as the maximum of the range to pick a random number from
+    float currentTotal = 0;
+    foreach (BoxSpawner boxSpawner in boxes)
     {
+      GameObject box = boxSpawner.box;
       MinigameBox minigameBox = box.GetComponent<MinigameBox>();
       UpgradeBox upgradeBox = box.GetComponent<UpgradeBox>();
       if ((!minigameBox && !upgradeBox) || (minigameBox && !MinigameBoxExists()) || (upgradeBox && !UpgradeBoxExists()))
       {
-        spawnOptions.Add(box);
+        currentTotal += boxSpawner.spawnRate;
+        spawnOptions.Add(new CumulativeBoxSpawner
+        {
+          box = box,
+          cumulativeSpawnRate = currentTotal
+        });
       }
     }
+
+    totalSpawnRate = currentTotal;
   }
 
   private void Spawn()
@@ -43,10 +69,24 @@ public class Spawner : MonoBehaviour
     float spawnHeight = transform.position.y;
     float min = transform.position.x - (spawnWidth / 2);
     float max = transform.position.x + (spawnWidth / 2);
-
-    GameObject box = spawnOptions[Random.Range(0, spawnOptions.Count)];
+    
+    GameObject box = SelectBox();
     Vector2 spawnPosition = new Vector2(Random.Range(min, max), spawnHeight);
     Instantiate(box, spawnPosition, Quaternion.identity);
+  }
+
+  private GameObject SelectBox()
+  {
+    float r = Random.Range(0, totalSpawnRate);
+    foreach (CumulativeBoxSpawner cumulativeBoxSpawner in spawnOptions)
+    {
+      if (r <= cumulativeBoxSpawner.cumulativeSpawnRate)
+      {
+        return cumulativeBoxSpawner.box;
+      }
+    }
+
+    return defaultBox;
   }
 
   private IEnumerator SetSpawnInterval()
